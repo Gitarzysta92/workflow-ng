2a35c475630f6c885d85ea2be4769cb55a3116b1
Michał Łukasiewicz <michal@threesixty.com>
2020-05-16 15:00:59 +0200

    Add switch button - resuable component

--- src/app/shared/components/switch-button/switch-button.component.ts
+++ src/app/shared/components/switch-button/switch-button.component.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'switch-button',
   templateUrl: './switch-button.component.html',
-  styleUrls: ['./switch-button.component.scss']
+  styleUrls: ['./switch-button.component.scss'],
+  animations: [
+    trigger('toggle', [
+      state('on', style({ transform: 'translate(100%)' })),
+      state('off', style({ transform: 'translate(0)' })),
+      transition('* => *', [ animate('0.3s ease-in-out') ]),
+    ]), 
+  ],
 })
 export class SwitchButtonComponent implements OnInit {
 
